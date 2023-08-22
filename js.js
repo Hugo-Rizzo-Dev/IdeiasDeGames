2af0635006620f195c4c8a50ddd99edab329acdf
Hugo Rizzo <rizzo_hugo@hotmail.com>
2023-08-22 13:47:07 -0300

    First att

---
+++ js.js
@@ -0,0 +1,7 @@
+/*$(window).scroll(function () {
+    if ($(window).scrollTop() === 0) {
+        $("nav").css("background-color", "rgba(0, 0, 0, 0)");
+    } else {
+        $("nav").css("background-color", "rgba(255, 0, 0, 0.3)");
+    }
+});
